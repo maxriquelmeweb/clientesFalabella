08ef4ec037b4a32d1bf098360961ba9c8b81fb01
mariquelmp <mariquelmp.3it@bancochile.cl>
2022-09-17 18:44:44 -0300

    [008] add - Se agrega icono

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+
+    <link rel="shortcut icon" type="image/png" href="{{ asset('img/icono_acobro.png') }}">
+
     <title>{{ config('app.name', 'Clientes Falabella') }}</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-    <!-- Scripts -->
+   
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
